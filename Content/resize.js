ec0162908bb9bcba86590f306033df59d33a9b04
Leo Choi <choimankin@gmail.com>
2018-08-26 14:05:00 +0800

    Initial

---
+++ Content/resize.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    resizeBodyTopMargin();
+});
+
+$(window).resize(function () {
+    resizeBodyTopMargin();
+});
+
+function resizeBodyTopMargin() {
+    var navHeight = $(".navbar").height();
+    if (navHeight > 70) {
+        $("body").css("padding-top", 130 + "px");
+    }
+    else {
+        $("body").css("padding-top", 70 + "px");
+    }
+}
